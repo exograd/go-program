0f8cdc1834a53f38c9b045a31f6bb94e94633d59
Nicolas Martyanoff <khaelin@gmail.com>
2021-11-23 20:36:45 +0100

    add build id parsing

---
+++ build_id.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2021 Nicolas Martyanoff <khaelin@gmail.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package program
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var (
+	buildIdRE *regexp.Regexp
+)
+
+func init() {
+	digit := `(0|(?:[1-9][0-9]*))`
+	version := `v` + digit + `.` + digit + `.` + digit
+	nbCommits := `([1-9][0-9]*)`
+	revision := `([a-z0-9]+)`
+
+	buildIdRE =
+		regexp.MustCompile(`^` + version +
+			`(?:-` + nbCommits + `-` + revision + `)?$`)
+}
+
+type BuildId struct {
+	Major int
+	Minor int
+	Patch int
+
+	NbCommits *int
+	Revision  *string
+}
+
+func (id BuildId) IsStable() bool {
+	return id.NbCommits == nil && id.Revision == nil
+}
+
+func (id BuildId) String() string {
+	s := fmt.Sprintf("v%d.%d.%d", id.Major, id.Minor, id.Patch)
+
+	if !id.IsStable() {
+		s += fmt.Sprintf("-%d-%s", *id.NbCommits, *id.Revision)
+	}
+
+	return s
+}
+
+func (id *BuildId) Parse(s string) error {
+	matches := buildIdRE.FindAllStringSubmatch(s, -1)
+	if len(matches) == 0 || len(matches[0]) == 0 {
+		return fmt.Errorf("invalid format")
+	}
+
+	id.Major, _ = strconv.Atoi(matches[0][1])
+	id.Minor, _ = strconv.Atoi(matches[0][2])
+	id.Patch, _ = strconv.Atoi(matches[0][3])
+
+	if len(matches[0][4]) > 0 {
+		n, _ := strconv.Atoi(matches[0][4])
+		id.NbCommits = &n
+
+		id.Revision = &matches[0][5]
+	}
+
+	return nil
+}
---
+++ build_id_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) 2021 Nicolas Martyanoff <khaelin@gmail.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package program
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersionParse(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		s  string
+		id BuildId
+	}{
+		{"v0.0.0",
+			BuildId{Major: 0, Minor: 0, Patch: 0}},
+		{"v1.2.3",
+			BuildId{Major: 1, Minor: 2, Patch: 3}},
+		{"v10.2.314",
+			BuildId{Major: 10, Minor: 2, Patch: 314}},
+		{"v1.2.3-17-f1d2d2f",
+			BuildId{Major: 1, Minor: 2, Patch: 3,
+				NbCommits: optionalInt(17),
+				Revision:  optionalString("f1d2d2f")}},
+	}
+
+	for _, test := range tests {
+		var id BuildId
+		if err := id.Parse(test.s); err != nil {
+			t.Errorf("cannot parse %q: %v", test.s, err)
+			continue
+		}
+
+		if assert.Equal(test.id, id, test.s) {
+			assert.Equal(test.s, id.String(), test.s)
+		}
+	}
+}
+
+func optionalInt(i int) *int {
+	return &i
+}
+
+func optionalString(s string) *string {
+	return &s
+}
